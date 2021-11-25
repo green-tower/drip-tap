256fc29ed6ea4afeacee76878d9c49485c197028
Gabriel Brogni Zaccaron <gabrielbrogni@hotmail.com>
2021-11-24 21:27:01 -0300

    Add bottom navigation

--- app/src/main/java/com/greentowersolution/driptap/MainActivity.kt
+++ app/src/main/java/com/greentowersolution/driptap/MainActivity.kt
@@ -3,10 +3,21 @@ package com.greentowersolution.driptap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.greentowersolution.driptap.ui.theme.DripTapTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DripTapTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -23,6 +34,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    val bottomNavigationItems = listOf(
+        BottomNavigationScreens.DripFormula
+    )
+
+    Scaffold(
+        bottomBar = {
+            AppBottomNavigation(navController, bottomNavigationItems)
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController,
+            startDestination = BottomNavigationScreens.DripFormula.route,
+            Modifier.padding(innerPadding)
+        ) {
+            composable(BottomNavigationScreens.DripFormula.route) { DripFormula() }
+        }
+    }
+}
+
+@Composable
+private fun DripFormula() {
+    Text(text = "It works!")//Todo remove this and create a file for the drip formula screen
+}
+
+@Composable
+private fun AppBottomNavigation(
+    navController: NavHostController,
+    items: List<BottomNavigationScreens>
+) {
+    BottomNavigation {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+
+        items.forEach { screen ->
+            BottomNavigationItem(
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                label = { Text(stringResource(id = screen.resourceId)) },
+                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+sealed class BottomNavigationScreens(val route: String, @StringRes val resourceId: Int) {
+    object DripFormula : BottomNavigationScreens("DripFormula", R.string.drip_formula)
 }
