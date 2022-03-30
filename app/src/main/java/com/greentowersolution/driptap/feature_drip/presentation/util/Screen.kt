0658f3de940309a9cf5e658c6880f7c16a0be3ce
Gabriel Brogni Zaccaron <gabrielbrogni@hotmail.com>
2022-03-29 23:05:23 -0300

    add drip validator screen

--- app/src/main/java/com/greentowersolution/driptap/feature_drip/presentation/util/Screen.kt
+++ app/src/main/java/com/greentowersolution/driptap/feature_drip/presentation/util/Screen.kt
@@ -6,4 +6,5 @@ import com.greentowersolution.driptap.R
 sealed class Screen(val route: String, @StringRes val resourceId: Int) {
     object DripFormula : Screen("drip_formula", R.string.drip_formula)
     object FrequencySettings : Screen("frequency_settings", R.string.frequency_settings)
+    object DripValidator : Screen("drip_validator", R.string.drip_validator)
 }
